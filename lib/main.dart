ce186b42150df10680a41abf5fabae14b133b71d
Carlos Augusto Aquino <carlos.aquino@aluno.uece.br>
2025-01-13 17:27:17 -0300

    Início Aula-6

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
-// Início Aula-5
-
+// Início Aula-6
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:teste1/screens/add_journal_screen/add_journal_screen.dart';
@@ -15,7 +14,6 @@ void main() {
   JournalService service = JournalService();
   // service.register(Journal.empty());
   // service.getAll();
-  // asyncStudy();
 }
 
 class MyApp extends StatelessWidget {
